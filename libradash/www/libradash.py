6e5f995c235790de092308e46587af8c46451754
Joelios <joel.mesmer@libracore.com>
2019-08-02 00:02:04 +0200

    sales order panel

--- libradash/www/libradash.py
+++ libradash/www/libradash.py
@@ -5,6 +5,8 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 import frappe.www.list
+from datetime import datetime
+
 
 no_cache = 1
 no_sitemap = 1
@@ -14,3 +16,11 @@ def get_context(context):
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 
 	context.show_sidebar=True
+	context['new_orders_qty'], context['month'] = new_orders()
+	
+def new_orders():
+	month = datetime.now().month
+	month_as_word = datetime.now().strftime('%B')
+	year = datetime.now().year
+	qty = frappe.db.sql("""SELECT COUNT(`name`) FROM `tabSales Order` WHERE `transaction_date` >= '{year}-{month}-01' AND `transaction_date` <= '{year}-{month}-31'""".format(year=year, month=month), as_list=True)[0][0]
+	return qty, month_as_word
